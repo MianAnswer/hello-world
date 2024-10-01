d23b13cf17b0e9d25c001f3619ad379bf2ec268e
MianAnswer <miananswer@hotmail.com>
2024-10-01 10:31:03 -0400

    Setup Express

--- api/src/app.ts
+++ api/src/app.ts
@@ -1,1 +1,16 @@
-console.log('hello, world')
+import express from 'express'
+
+const app = express()
+const PORT = process.env.PORT || 8080
+
+app.get('/', (req, res) => {
+  res.status(200).send(req.url)
+})
+
+app.use((req, res) => {
+  res.status(404).end()
+})
+
+app.listen(PORT, () => {
+  console.log(`http://localhost:${PORT}`)
+})
