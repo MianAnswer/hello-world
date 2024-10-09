3dca45afe6eafc0f7d49453637522df9e8d368f6
Mian Answer <mian.answer@accelya.com>
2024-10-09 17:00:14 -0400

    Update status code in response of POST /user request

--- api/src/routes/user-router.ts
+++ api/src/routes/user-router.ts
@@ -13,7 +13,7 @@ userRouter.post('/', (req, res) => {
   if (error) {
     res.status(400).send(error.details[0].message)
   } else {
-    res.status(200).send(value)
+    res.status(201).send(value)
   }
 })
 
