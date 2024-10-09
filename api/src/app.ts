a63938772847385981b1ebaaa72052f0ee6811b0
Mian Answer <mian.answer@accelya.com>
2024-10-09 12:54:11 -0400

    Parse json payload and handle parsing error

--- api/src/app.ts
+++ api/src/app.ts
@@ -1,4 +1,6 @@
-import express from 'express'
+import express, {
+  Request, Response, NextFunction, ErrorRequestHandler,
+} from 'express'
 import dotenv from 'dotenv'
 import router from './routes/router'
 
@@ -7,7 +9,20 @@ dotenv.config()
 const app = express()
 const PORT = process.env.PORT || 8080
 
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
+
 app.use('/', router)
+
+// Error-handling middleware for JSON parsing errors
+app.use((err: ErrorRequestHandler, req: Request, res: Response, next: NextFunction) => {
+  if (err instanceof SyntaxError && 'body' in err) {
+    console.error('Bad JSON')
+    res.status(400).send({ error: 'Invalid JSON' })
+  } else {
+    next(err)
+  }
+})
 
 app.use((req, res) => {
   res.status(404).end()
@@ -15,4 +30,14 @@ app.use((req, res) => {
 
 app.listen(PORT, () => {
   console.log(`http://localhost:${PORT}`)
+})
+
+process.on('unhandledRejection', (error) => {
+  console.error(error)
+  process.exit(1)
+})
+
+process.on('uncaughtException', (error) => {
+  console.error(error)
+  process.exit(1)
 })
